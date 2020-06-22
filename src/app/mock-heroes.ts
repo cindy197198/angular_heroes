5ee1e1f0f1b5d20eec8cdda851f99cb64260ad8f
cindy197198 <trangnguyen197198@gmail.com>
2020-06-22 21:11:06 +0700

    init project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero} from './hero';
+import { constants } from 'buffer';
+
+export const HEROES: Hero[] = [
+  {id: 11, name : 'Dr Nice' },
+  {id: 12, name : 'Yoona Im' },
+  {id: 13, name : 'Min Ho' },
+  {id: 14, name : 'Hyeri' },
+  {id: 15, name : 'Ho Jun' },
+  {id: 16, name : 'Tae Yeon' },
+  {id: 17, name : 'IU' },
+  {id: 18, name : 'Chang Wook' },
+];
