c07c64a93fa23e7353c7e1a8bad3a876d8e22908
cindy197198 <trangnguyen197198@gmail.com>
2020-06-23 13:04:28 +0700

    routing

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+export class MessageComponent implements OnInit {
+
+  constructor(public messageService: MessageService) { }//The messageService property must be public because you're going to bind to it in the template.
+
+  ngOnInit(): void {
+  }
+
+}
